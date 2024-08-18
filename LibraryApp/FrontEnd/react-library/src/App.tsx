c3e2a0b9cef42a7284a626fa04a2754d932e0b36
jayasimhag1reddy <jayasimhag1reddy@gmail.com>
2024-08-17 19:59:01 -0500

    Added project setup folders

---
+++ LibraryApp/FrontEnd/react-library/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
+
+function App() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
