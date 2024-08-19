f9238e95ac0fd826fa2feab4f882950e23e4bf54
jayasimhag1reddy <jayasimhag1reddy@gmail.com>
2024-08-19 16:32:02 -0500

    Added ExploreTopBooks frontend feature

--- LibraryApp/FrontEnd/react-library/src/App.tsx
+++ LibraryApp/FrontEnd/react-library/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './Layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './Layouts/HomePage/Carousel';
 
 function App() {
   return (
+    <>
     <Navbar/>
+    <ExploreTopBooks/>
+    <Carousel/>
+    </>
   );
 }
 
